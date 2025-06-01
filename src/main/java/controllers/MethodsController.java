3cab122cd2c73efa384fcb82432291644e52fa1e
Luís Gustavo <luisgustavo.teodoro52@gmail.com>
2025-06-01 01:47:10 -0300

    Correções

---
+++ src/main/java/controllers/MethodsController.java
@@ -0,0 +1,23 @@
+package controllers;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/methods")
+public class MethodsController extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setHeader("Allow", "GET, POST, HEAD, OPTIONS");
+	    resp.setContentType("text/plain");
+
+	    
+	}
+}
